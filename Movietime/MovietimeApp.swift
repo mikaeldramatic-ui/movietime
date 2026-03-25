d3de75a8ec222e9c128c2b0d3b37759953745bee
Micke <mikael.dramatic@gmail.com>
2026-03-25 13:31:59 +0100

    first commit

---
+++ Movietime/MovietimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovietimeApp.swift
+//  Movietime
+//
+//  Created by Mikael Engvall on 2026-03-25.
+//
+
+import SwiftUI
+
+@main
+struct MovietimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
